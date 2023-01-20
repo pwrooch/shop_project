80c021016d4e7061739f4a8838e8a90032df7e18
rooch <pongsiri.bestout@hotmail.com>
2023-01-20 17:49:34 +0700

    up project git

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,23 +8,27 @@
                  <div class="row">
                     @foreach ($productsView as $item)
                     <div class="col-3">
-                        <div class="card mt-4">
-                            <img src="https://images.pexels.com/photos/139764/pexels-photo-139764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top" alt="...">
+                        <div class="card mt-4" style="width: 20rem; ">
+                            <img src="https://images.pexels.com/photos/13428021/pexels-photo-13428021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">{{ $item->name}}</h5>
-                                <p class="card-text ">{{ $item->details}}</p>
+                                <div class="container my-2">
+                                    <div class="">
+                                        <h4 class="card-title">{{ $item->name }}</h4>
+                                    </div>
+                                    <div class="">
+                                        <p class="card-text ">{{ $item->details }}</p>
+                                    </div>
+                                </div>
                                 <div class="container">
-                                    <div class="row align-items-end">
+                                    <div class="row">
                                         <div class="col">
-                                            <p class="card-text">{{ $item->price}} Baht</p>
+                                            <p class="card-text">{{ $item->price }} Baht</p>
                                         </div>
-                                        <div class="col">
-                                            <a href="{{ route('products.edit', $item->id)}}" class="btn btn-outline-primary">Edit</a>
-                                        </div>
-                                        <div class="col">
-                                            <form action="{{ route('products.destroy', $item->id)}}" method="POST">
+                                        <div class="row mt-2">
+                                            <form action="{{ route('products.destroy', $item->id) }}" method="POST">
                                                 @csrf
                                                 @method('delete')
+                                                <a href="{{ route('products.edit', $item->id)}}" class="btn btn-outline-primary">Edit</a>
                                                 <button type="submit" class="btn btn-outline-danger">Delete</button>
                                             </form>
                                         </div>
